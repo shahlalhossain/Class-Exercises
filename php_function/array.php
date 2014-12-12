97456e9f8a878b7e633aba998f4aaadabebe937e
Shahlal47 <shahlal_947@yahoo.com>
2014-12-11 23:47:03 -0500

    array() example

---
+++ php_function/array.php
@@ -0,0 +1,8 @@
+
+<?php
+$fruits = array (
+    "fruits"  => array("a" => "orange", "b" => "banana", "c" => "apple"),
+    "numbers" => array(1, 2, 3, 4, 5, 6),
+    "holes"   => array("first", 5 => "second", "third")
+);
+?>
