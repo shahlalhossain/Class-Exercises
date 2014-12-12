f8879ce05efcc03f4776cdacaf6cb55514f886f6
Shahlal47 <shahlal_947@yahoo.com>
2014-12-11 23:48:06 -0500

    arsort() example

---
+++ php_function/arsort.php
@@ -0,0 +1,9 @@
+
+<?php
+$fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
+arsort($fruits);
+foreach ($fruits as $key => $val) {
+    echo "$key = $val\n";
+}
+?>
+
