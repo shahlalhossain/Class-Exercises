02a112f29b28bff7e8e9ce04ac2396c64b095c13
Shahlal47 <shahlal_947@yahoo.com>
2014-12-11 22:59:03 -0500

    array_replace() example

---
+++ php_function/array_replace.php
@@ -0,0 +1,9 @@
+
+<?php
+$base = array("orange", "banana", "apple", "raspberry");
+$replacements = array(0 => "pineapple", 4 => "cherry");
+$replacements2 = array(0 => "grape");
+
+$basket = array_replace($base, $replacements, $replacements2);
+print_r($basket);
+?>
