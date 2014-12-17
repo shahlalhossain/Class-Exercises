0bf4f8b275e8d261fb4add78a45b4a2d735d3803
Shahlal47 <shahlalhossain@gmail.com>
2014-12-17 20:22:10 +0600

    edit something in coding

--- php_function/array_search.php
+++ php_function/array_search.php
@@ -4,4 +4,7 @@ $array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
 
 $key = array_search('green', $array); // $key = 2;
 $key = array_search('red', $array);   // $key = 1;
+
+echo "$key";
+
 ?>
