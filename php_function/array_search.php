c028f82d77b0b92031fd6e3bab5e9291b74bb368
Shahlal47 <shahlalhossain@gmail.com>
2014-12-17 22:38:21 +0600

    explode() examples

--- php_function/array_search.php
+++ php_function/array_search.php
@@ -2,9 +2,10 @@
 <?php
 $array = array(0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red');
 
-$key = array_search('green', $array); // $key = 2;
-$key = array_search('red', $array);   // $key = 1;
+$key1 = array_search('green', $array); // $key = 2;
+$key2 = array_search('red', $array);   // $key = 1;
 
-echo "$key";
+echo "$key1" . "<br>";
+echo "$key2";
 
 ?>
--- php_function/count.php
+++ php_function/count.php
@@ -4,13 +4,16 @@ $a[0] = 1;
 $a[1] = 3;
 $a[2] = 5;
 $result = count($a);
-// $result == 3
+
+print_r($result); // $result == 3
 
 $b[0]  = 7;
 $b[5]  = 9;
 $b[10] = 11;
 $result = count($b);
 // $result == 3
+
+
 
 $result = count(null);
 // $result == 0
