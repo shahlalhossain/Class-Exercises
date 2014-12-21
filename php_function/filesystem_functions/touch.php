50d32113709447c33edd9589437c3c8b131edefa
Shahlal47 <shahlalhossain@gmail.com>
2014-12-22 02:37:58 +0600

    touch() example

---
+++ php_function/filesystem_functions/touch.php
@@ -0,0 +1,11 @@
+
+<?php
+//example #1
+
+
+if (touch($filename)) {
+    echo $filename . ' modification time has been changed to present time';
+} else {
+    echo 'Sorry, could not change modification time of ' . $filename;
+}
+?>
