4a4cdecb3ba96abe20970f59e2656eadcc9f8b9d
Shahlal47 <shahlalhossain@gmail.com>
2014-12-22 01:09:02 +0600

    Example #1+2+3 isset() Examples

---
+++ var_dump/isset.php
@@ -0,0 +1,74 @@
+
+//Example #1 isset() Examples
+
+<?php
+
+$var = '';
+
+// This will evaluate to TRUE so the text will be printed.
+if (isset($var)) {
+    echo "This var is set so I will print.";
+}
+
+// In the next examples we'll use var_dump to output
+// the return value of isset().
+
+$a = "test";
+$b = "anothertest";
+
+var_dump(isset($a));      // TRUE
+var_dump(isset($a, $b)); // TRUE
+
+unset ($a);
+
+var_dump(isset($a));     // FALSE
+var_dump(isset($a, $b)); // FALSE
+
+$foo = NULL;
+var_dump(isset($foo));   // FALSE
+
+?>
+
+
+
+
+//Example #2 isset() Examples
+
+
+
+<?php
+
+$a = array ('test' => 1, 'hello' => NULL, 'pie' => array('a' => 'apple'));
+
+var_dump(isset($a['test']));            // TRUE
+var_dump(isset($a['foo']));             // FALSE
+var_dump(isset($a['hello']));           // FALSE
+
+// The key 'hello' equals NULL so is considered unset
+// If you want to check for NULL key values then try:
+var_dump(array_key_exists('hello', $a)); // TRUE
+
+// Checking deeper array values
+var_dump(isset($a['pie']['a']));        // TRUE
+var_dump(isset($a['pie']['b']));        // FALSE
+var_dump(isset($a['cake']['a']['b']));  // FALSE
+
+?>
+
+
+
+
+
+//Example #3 isset() Examples
+
+
+
+<?php
+$expected_array_got_string = 'somestring';
+var_dump(isset($expected_array_got_string['some_key']));
+var_dump(isset($expected_array_got_string[0]));
+var_dump(isset($expected_array_got_string['0']));
+var_dump(isset($expected_array_got_string[0.5]));
+var_dump(isset($expected_array_got_string['0.5']));
+var_dump(isset($expected_array_got_string['0 Mostel']));
+?>
