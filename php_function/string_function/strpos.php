d368f4460651aaf1b51153681f8b6bd03476f1ab
Shahlal Hossain <shahlalhossain@gmail.com>
2015-03-08 10:26:29 +0600

    Testing the PHP's Build in Function strpos()

--- php_function/string_function/strpos.php
+++ php_function/string_function/strpos.php
@@ -1,15 +1,14 @@
-
 <?php
-$mystring = 'abc';
-$findme   = 'a';
-$pos = strpos($mystring, $findme);
+$myString = 'abc';
+$findMe   = 'a';
+$pos = strpos($myString, $findMe);
 
 // Note our use of ===.  Simply == would not work as expected
 // because the position of 'a' was the 0th (first) character.
 if ($pos === false) {
-    echo "The string '$findme' was not found in the string '$mystring'";
+    echo "The string '$findMe' was not found in the string '$myString'";
 } else {
-    echo "The string '$findme' was found in the string '$mystring'";
+    echo "The string '$findMe' was found in the string '$myString'";
     echo " and exists at position $pos";
 }
 ?>
