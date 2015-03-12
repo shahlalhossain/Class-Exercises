7274ead214b407993ac41c27a3661b1f14d3968e
Shahlal Hossain <shahlalhossain@gmail.com>
2015-03-12 10:30:54 +0600

    Test the PHP's Build in Function substr()

--- php_function/string_function/substr.php
+++ php_function/string_function/substr.php
@@ -1,15 +1,8 @@
-
 <?php
 $rest = substr("abcdef", -1);    // returns "f"
 $rest = substr("abcdef", -2);    // returns "ef"
 $rest = substr("abcdef", -3, 1); // returns "d"
-?>
 
-
-
-
-
-<?php
 $rest = substr("abcdef", 0, -1);  // returns "abcde"
 $rest = substr("abcdef", 2, -1);  // returns "cde"
 $rest = substr("abcdef", 4, -4);  // returns false
